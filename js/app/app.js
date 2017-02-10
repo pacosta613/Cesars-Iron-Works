0fcb582fbb4f5cff69aa2097995db8ddcbfc94d2
pacosta613 <pedro.acosta613@gmail.com>
2017-02-10 17:28:50 -0500

    Added states for created view files for balconies, cellar, fence, fire, porch, railing, stair, and window.

--- js/app/app.js
+++ js/app/app.js
@@ -11,23 +11,63 @@
     $stateProvider
       .state("home", {
         url: "/home",
-        templateUrl: "js/views/home.html",
+        templateUrl: "js/views/navbar/home.html",
         controller: "HomeController"
       })
   
       .state("about", {
         url: "/about",
-        templateUrl: "js/views/about.html"
+        templateUrl: "js/views/navbar/about.html"
       })
   
       .state("product", {
         url: "/product",
-        templateUrl: "js/views/product.html"
+        templateUrl: "js/views/navbar/product.html"
       })
 
       .state("contact", {
         url: "/contact",
-        templateUrl: "js/views/contact.html"
+        templateUrl: "js/views/navbar/contact.html"
+      })
+
+      .state("railing", {
+        url: "/railing",
+        templateUrl: "js/views/sidebar/railing.html"
+      })
+
+      .state("window", {
+        url: "/window",
+        templateUrl: "js/views/sidebar/window.html"
+      })
+
+      .state("stair", {
+        url: "/stair",
+        templateUrl: "js/views/sidebar/stair.html"
+      })
+
+      .state("fence", {
+        url: "/fence",
+        templateUrl: "js/views/sidebar/fence.html"
+      })
+
+      .state("cellar", {
+        url: "/cellar",
+        templateUrl: "js/views/sidebar/cellar.html"
+      })
+
+      .state("balconies", {
+        url: "/balconies",
+        templateUrl: "js/views/sidebar/balconies.html"
+      })
+
+      .state("fire", {
+        url: "/fire",
+        templateUrl: "js/views/sidebar/fire.html"
+      })
+
+      .state("porch", {
+        url: "/porch",
+        templateUrl: "js/views/sidebar/porch.html"
       });
   });
 
